9b6b092c58ab4218e3e80ee963c9a62a91f02a85
Correct Name <adityaj2003@gmail.com>
2023-05-24 00:36:51 -0700

    LR and RF Baseline visualisation

--- MakeData.py
+++ MakeData.py
@@ -116,3 +116,69 @@ negative_indices = y_train == -1
 
 
 
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pyplot as plt
+import numpy as np
+
+import matplotlib.patches as mpatches
+
+
+def plot_random_forest(x_train, y_train, x_test, y_test):
+    B_values = [0.1, 0.3, 1, 3, 9]
+    alpha_values = [1 / 2, 1]
+    accuracy_values_rf = []
+    accuracy_values_lr = []
+
+    for alpha in alpha_values:
+        accuracy_alpha_rf = []
+        accuracy_alpha_lr = []
+        for B in B_values:
+            # Add Tsybakov noise to the training labels
+            noisy_y_train = add_noise(x_train, y_train, alpha, B)
+
+            # Train and evaluate Random Forest
+            rf_model = RandomForestClassifier(max_depth=20, random_state=0)
+            rf_model.fit(x_train, noisy_y_train)
+            y_test_pred_rf = rf_model.predict(x_test)
+            accuracy_rf = accuracy_score(y_test, y_test_pred_rf)
+            accuracy_alpha_rf.append(accuracy_rf)
+
+            # Train and evaluate Logistic Regression
+            lr_model = LogisticRegression(random_state=0)
+            lr_model.fit(x_train, noisy_y_train)
+            y_test_pred_lr = lr_model.predict(x_test)
+            accuracy_lr = accuracy_score(y_test, y_test_pred_lr)
+            accuracy_alpha_lr.append(accuracy_lr)
+
+        accuracy_values_rf.append(accuracy_alpha_rf)
+        accuracy_values_lr.append(accuracy_alpha_lr)
+
+    # Reshape the accuracy arrays
+    accuracy_values_rf = np.array(accuracy_values_rf).reshape(len(alpha_values), len(B_values))
+    accuracy_values_lr = np.array(accuracy_values_lr).reshape(len(alpha_values), len(B_values))
+    B_values, alpha_values = np.meshgrid(B_values, alpha_values)
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+
+    # Plot Random Forest accuracy values
+    ax.plot_surface(B_values, alpha_values, accuracy_values_rf, color='blue', alpha=0.7)
+
+    # Plot Logistic Regression accuracy values
+    ax.plot_surface(B_values, alpha_values, accuracy_values_lr, color='red', alpha=0.7)
+
+    ax.set_xlabel('B')
+    ax.set_ylabel('Alpha')
+    ax.set_zlabel('Accuracy')
+
+    # Create proxy artists for legend
+    proxy_rf = mpatches.Patch(color='blue', label='Random Forest')
+    proxy_lr = mpatches.Patch(color='red', label='Logistic Regression')
+
+    # Add a legend
+    ax.legend(handles=[proxy_rf, proxy_lr])
+
+    plt.show()
+
+
+plot_random_forest(x_train, y_train, x_test, y_test)
