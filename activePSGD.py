1738561bc37f7a0a9b9e2e0214e957d38e06930e
Aditya Jadhav <adityaj2003@gmail.com>
2023-06-29 13:58:21 +0530

    Refactoring

--- activePSGD.py
+++ activePSGD.py
@@ -1,3 +1,4 @@
+import argparse
 import sys
 import numpy as np
 import matplotlib.pyplot as plt
@@ -22,6 +23,15 @@ import math
 import traceback
 
 
+parser = argparse.ArgumentParser()
+
+parser.add_argument('--alpha', type=float, default=0.5, help='First input integer')
+parser.add_argument('--B', type=float, default=0.3, help='Second input integer')
+
+args = parser.parse_args()
+
+
+
 x_train = np.load('x_train.npy')
 x_val = np.load('x_val.npy')
 x_test = np.load('x_test.npy')
@@ -39,8 +49,6 @@ delta = 0.1
 A = 1
 alpha = 0.5
 label_used_array = [False]*TRAIN_SET_SIZE
-num_labels_accessed = 0
-index = 0
 O_constant = 50
 Theta_constant = 10
 
@@ -50,12 +58,18 @@ rho = Theta_constant * (1/A)**((2*(1-alpha))/(3*alpha-1)) * theta**((2*(1-alpha)
 S = np.log(6 / delta)
 
 print("sigma: ", sigma)
-def sample_x_from_DX(index):
-    x = x_train[index, :]
-    return x
 
-def query_labeling_oracle(index):
-    return y_train[index]
+def single_gauss(d):
+    vecs = np.zeros((1, d))
+    if np.random.uniform() > 0.5:
+        vecs[0, :] = np.random.multivariate_normal([0]*d, cov1)
+    else:
+        vecs[0, :] = np.random.multivariate_normal([0]*d, cov2)
+
+    x_train = vecs
+    y_train = (vecs[0, 1] > 0).astype(int) * 2 - 1
+
+    return x_train, y_train
 
 def phi_prime_of_sigma_t(w, x):
     i = np.dot(w, x) / np.linalg.norm(w)
@@ -64,52 +78,38 @@ def phi_prime_of_sigma_t(w, x):
 
 
 def ACTIVE_FO(w):
-    global index
-    global TRAIN_SET_SIZE
-    if (label_used_array[index]):
-        index = (index + 1) % TRAIN_SET_SIZE
-        return np.zeros_like(w)
-    x = sample_x_from_DX(index)
+    x,y = single_gauss(d)
     q_wx = sigma * phi_prime_of_sigma_t(w, x)
     Z = bernoulli.rvs(q_wx)
-
     if Z == 1:
-        global num_labels_accessed
         num_labels_accessed += 1
-        y = query_labeling_oracle(index)
         w_norm = np.linalg.norm(w)
         h_wxy = -1/sigma * y * (x/w_norm**2 - np.dot(w, x) * w / w_norm**3)
-        label_used_array[index] = True
-        index = (index+1)%TRAIN_SET_SIZE
-        assert np.isclose(np.dot(h_wxy, w), 0)
+        assert np.isclose(np.dot(h_wxy, w), 0), 1
         return h_wxy
     else:
-        index = (index+1)%TRAIN_SET_SIZE
-        return np.zeros_like(w)
+        return np.zeros_like(w), 0
 
 iterate_accuracies_noisy = []
 iterate_accuracies = []
 iterate_labels_used = []
+
 def ACTIVE_PSGD(N, beta):
-    global num_labels_accessed
     w1 = np.random.normal(size=d)
     w1 = w1 / np.linalg.norm(w1)
     print("w1", w1)
     w = [None]*(N+1)
     w[0] = w1
-    label_used_array = [False] * TRAIN_SET_SIZE
-    index = 0
     for i in range(1, N+1):
         gi = ACTIVE_FO(w[i-1])
-        label_used_array[index] = True
         vi = w[i-1] - beta*gi
         w[i] = vi / np.linalg.norm(vi)
         predictions = np.dot(x_test, w[i])
-        for i in range(0,len(predictions)):
-            if predictions[i] < 0.0:
-                predictions[i] = -1
+        for j in range(0,len(predictions)):
+            if predictions[j] < 0.0:
+                predictions[j] = -1
             else:
-                predictions[i] = 1
+                predictions[j] = 1
         iterate_accuracies_noisy.append(accuracy_score(y_test, predictions))
         iterate_accuracies.append(accuracy_score(y_test_orig,predictions))
         iterate_labels_used.append(num_labels_accessed)
@@ -124,14 +124,10 @@ def TNC_Learning_New(epsilon, delta):
     w[0] = w1
     print("w1", w1)
     i = 1
-    global index
-    global label_used_array
     global num_labels_accessed
-    index = 0
     num_labels_accessed = 0
-    label_used_array = [False] * TRAIN_SET_SIZE
-    while num_labels_accessed < 1000:
-        gi = ACTIVE_FO(w[i-1])
+    while li < 1000:
+        gi, li = ACTIVE_FO(w[i-1])
         vi = w[i-1] - beta*gi
         w[i] = vi / np.linalg.norm(vi)
         predictions = np.dot(x_test, w[i])
@@ -143,8 +139,9 @@ def TNC_Learning_New(epsilon, delta):
         iterate_accuracies_noisy.append(accuracy_score(y_test, predictions))
         iterate_accuracies.append(accuracy_score(y_test_orig,predictions))
         iterate_labels_used.append(num_labels_accessed)
-        i += 1
+        i += li
 
+#Store
 
 
 
@@ -154,48 +151,48 @@ N = d / (sigma**2 * rho**4)
 beta = rho**2 * sigma**2 / d
 print("beta: ", beta)
 
-def TNC_learning(epsilon, delta,N):
-    num_labels_accessed = 0
-    index = 0
-    # Constants for Big O and Big Theta
-    O_constant = 50
-    Theta_constant = 50
-    theta = O_constant * ((1 / np.log2(1 / epsilon)) ** 2) * (epsilon / 2)
-    sigma = Theta_constant * (1 / A) ** ((1 - alpha) / (3 * alpha - 1)) * theta ** ((2 * alpha) / (3 * alpha - 1))
-    rho = Theta_constant * (1 / A) ** ((2 * (1 - alpha)) / (3 * alpha - 1)) * theta ** (
-                (2 * (1 - alpha)) / (3 * alpha - 1))
-    S = np.log(6 / delta)
-    w_s = []
-    for s in range(math.ceil(S)):
-        beta = rho ** 2 * sigma ** 2 / d
-        w = ACTIVE_PSGD(N, beta)
-        w_s.append(w)
+# def TNC_learning(epsilon, delta,N):
+#     num_labels_accessed = 0
+#     index = 0
+#     # Constants for Big O and Big Theta
+#     O_constant = 50
+#     Theta_constant = 50
+#     theta = O_constant * ((1 / np.log2(1 / epsilon)) ** 2) * (epsilon / 2)
+#     sigma = Theta_constant * (1 / A) ** ((1 - alpha) / (3 * alpha - 1)) * theta ** ((2 * alpha) / (3 * alpha - 1))
+#     rho = Theta_constant * (1 / A) ** ((2 * (1 - alpha)) / (3 * alpha - 1)) * theta ** (
+#                 (2 * (1 - alpha)) / (3 * alpha - 1))
+#     S = np.log(6 / delta)
+#     w_s = []
+#     for s in range(math.ceil(S)):
+#         beta = rho ** 2 * sigma ** 2 / d
+#         w = ACTIVE_PSGD(N, beta)
+#         w_s.append(w)
 
-    M1 = 100 
+#     M1 = 100 
 
-    g_s = []
-    for s in range(math.ceil(S)):
-        g_s_i = []
-        for _ in range(M1):
-            g_s_i.append(ACTIVE_FO(w_s[s]))
-        g_s.append(np.mean(g_s_i, axis=0)) 
+#     g_s = []
+#     for s in range(math.ceil(S)):
+#         g_s_i = []
+#         for _ in range(M1):
+#             g_s_i.append(ACTIVE_FO(w_s[s]))
+#         g_s.append(np.mean(g_s_i, axis=0)) 
 
-    s_star = np.argmin(np.linalg.norm(g_s, axis=1))
-    w_tilde = w_s[s_star]
+#     s_star = np.argmin(np.linalg.norm(g_s, axis=1))
+#     w_tilde = w_s[s_star]
 
-    errors = []
-    for w in [w_tilde, -w_tilde]:
-        error = 0
-        for i in range(len(x_val)):
-            x_i = x_val[i]
-            y_i = y_val[i]
-            if np.sign(np.dot(w, x_i)) != y_i:
-                error += 1
-        errors.append(error / len(x_val))
+#     errors = []
+#     for w in [w_tilde, -w_tilde]:
+#         error = 0
+#         for i in range(len(x_val)):
+#             x_i = x_val[i]
+#             y_i = y_val[i]
+#             if np.sign(np.dot(w, x_i)) != y_i:
+#                 error += 1
+#         errors.append(error / len(x_val))
 
-    w_hat = [w_tilde, -w_tilde][np.argmin(errors)]
+#     w_hat = [w_tilde, -w_tilde][np.argmin(errors)]
 
-    return w_hat
+#     return w_hat
 
 
 
@@ -222,10 +219,10 @@ for i, sigmaIterate in enumerate(sigmas):
         try:
             sigma = sigmaIterate
             beta = betaIterate
-            N = d / (sigma**2 * rho**4)
             iterate_accuracies = []
             iterate_labels_used = []
             TNC_Learning_New(epsilon, delta)
+            #Make TNC_LEARNING_NEW return iterate_labels_used and iterate_accuracies instead of declaring them as global variables
             axs[l,i].plot(iterate_labels_used, iterate_accuracies, label="Test Accuracy")
             axs[l,i].set_title(f"Sigma = {sigma}, Beta = {beta}")
             axs[l,i].set_xlabel("Beta")
