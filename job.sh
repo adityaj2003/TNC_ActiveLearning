12d82ed4d6653156b21ac7fe7c4ee2912a334e65
Aditya Jadhav <adityaj2003@gmail.com>
2023-07-18 21:47:26 +0530

    HPC sbatch script

---
+++ job.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python3 activePSGD.py --sigma $1 --beta $2
--- makeData.sh
+++ makeData.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Define the list of sigma values and beta values
+# Define the list of sigma and beta values
 sigma_values=(0.001 0.003 0.01 0.03 0.1 0.3 1 3)
 beta_values=(0.001 0.003 0.01 0.03 0.1 0.3 1 3)
 
@@ -8,6 +8,9 @@ beta_values=(0.001 0.003 0.01 0.03 0.1 0.3 1 3)
 for sigma in "${sigma_values[@]}"; do
   for beta in "${beta_values[@]}"; do
     # Submit a job to SLURM for each combination
-    sbatch --job-name=activePSGD_"$sigma"_"$beta" --output=activePSGD_"$sigma"_"$beta".out --wrap="python activePSGD.py --sigma $sigma --beta $beta"
+    job_name="activePSGD_${sigma}_${beta}"
+    output_file="activePSGD_${sigma}_${beta}.out"
+    sbatch --job-name="$job_name" --output="$output_file" job.sh "$sigma" "$beta"
   done
 done
+
